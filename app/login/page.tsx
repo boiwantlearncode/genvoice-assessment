49cf8f28824e0748e8903a27377a6ae9056a94ef
boiwantlearncode <ibetank@gmail.com>
2024-10-20 16:15:15 +0800

    fixed for mobile

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "../components/LoginForm";
 export default function Home() {
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="container rounded-lg bg-gray-50 relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-1 lg:px-0">
+      <div className="container rounded-lg bg-gray-50 relative h-[800px] px-4 flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-1 lg:px-0">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
